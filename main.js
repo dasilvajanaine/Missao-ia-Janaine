af00b7f0890f86d6a3dd052d6f338f0e0162e235
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-10-21 14:48:30 -0300

    2110

--- main.js
+++ main.js
@@ -75,10 +75,16 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -86,12 +92,25 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click', function () {
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+let resposta
 
 mostraPergunta();
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Se fosse possivel";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
